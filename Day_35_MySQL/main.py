cfa78692a354122121233533a56a921202323134
phoenix-295 <70465964+phoenix-295@users.noreply.github.com>
2020-11-21 13:05:10 +0530

    Flask With MySQL

--- Day_35_MySQL/main.py
+++ Day_35_MySQL/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, jsonify, url_for
+from flask import render_template, Flask, request, redirect, url_for, jsonify
 from forms1 import Post_form
 from modal1 import app, blog_details13, db
 from flask_restful import Resource, Api
@@ -15,26 +15,26 @@ post = [{'blogtitle': "My first post", 'blogcontent': "Hello how are you", 'date
 
 @app.route('/', methods=['GET', 'POST'])
 def display():
-    posts = blog_details13.all()
-    return render_template('display1.html', posts=posts)
+    posts = blog_details13.query.all()
+    return render_template('display.html', posts=posts)
 
 
-@app.route('/post', methods=['GET', 'Post'])
+@app.route("/post", methods=['GET', 'POST'])
 def post1():
     print("Hello")
     form = Post_form(request.form)
     if form.validate():
-        post1 = blog_details13(form.title.data, form.blogcontent)
+        post1 = blog_details13(form.title.data, form.content.data)
         db.session.add(post1)
         db.session.commit()
-        # return re
-    print(form.data)
+        return redirect(url_for('display'))
     return render_template('create_post.html', form=form, legend='Write Post today')
 
 
-@app.route('/json/<id>', methods=['GET', 'Post'])
-def jsondata():
-    data = blog_details13.query.filterby()
+class Data1(Resource):
+    def get(self, id1):
+        data = blog_details13.query.filter_by(blogid=id1).first()
+        return {'about': str(data)}
 
 
 if __name__ == '__main__':
--- Day_35_MySQL/modal1.py
+++ Day_35_MySQL/modal1.py
@@ -5,8 +5,9 @@ from datetime import datetime
 
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite4'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@127.0.0.1/mohit'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:mysql@localhost/testdb'
 app.config['SECRET_KEY'] = "random string"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
@@ -15,15 +16,15 @@ class blog_details13(db.Model):
     blogid = db.Column(db.Integer, primary_key=True)  # Automatic increment
     blogtitle = db.Column(db.String(100), nullable=False)
     blogcontent = db.Column(db.String(500), nullable=False)
-    date1 = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    # date1 = db.Column(db.DateTime, nullable=True, default=datetime.utcnow)
 
-    def __init__(self, blogtitle, blogcontent, date1):
+    def __init__(self, blogtitle, blogcontent):
         self.blogtitle = blogtitle
         self.blogcontent = blogcontent
-        self.date1 = date1
+        # self.date1 = date1
 
     def __repr__(self):
-        pass
+        return f"({self.blogtitle}, {self.blogcontent})"
 
 
 if __name__ == '__main__':
